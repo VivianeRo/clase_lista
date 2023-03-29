50ac0ceee6f055d885e6a330404dc727628d3332
Viviane Rodriguez <vivianerg84@gmail.com>
2023-03-29 14:22:15 -0300

    SE MODIFICO EL CODIGO

--- lista.py
+++ lista.py
@@ -3,3 +3,14 @@ lista2= ['Bye', 'ciao', 'Agur', 'Adieu']
 
 print (lista1)
 print(lista2)
+lista1.append(1234)
+lista1.append('Hola')
+print(lista1)
+
+lista2.append('Adios')
+lista2.append(1234)
+print(lista2)
+
+lista3 = lista1[0:-1]
+print(lista3)
+print(lista1)
